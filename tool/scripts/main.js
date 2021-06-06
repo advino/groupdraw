76122488d71a611dfee53304dd6c4d280fd02cd4
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-06-06 10:10:35 -0700

    tool switching based on network size

--- tool/scripts/main.js
+++ tool/scripts/main.js
@@ -7,21 +7,53 @@ beaker.hyperdrive.getInfo('hyper://c90bd39d0cb8252fb43fe2d7aa13555a581c0fd22a3b5
 
     console.log(info);
     window.versionNum.innerHTML = info.version;
+    document.title = info.title;
+    window.siteUrl.innerHTML = info.url;
+
+    peerEvents.addEventListener('join', e => {
+
+        peerIds.add(e.peerId);
+        window.peercount.innerHTML = peerIds.size;
+        window.peerSize = peerIds.size;
+    });
+    
+    peerEvents.addEventListener('leave', e => {
+    
+        peerIds.delete(e.peerId);
+        window.peercount.innerHTML = peerIds.size;
+        window.peerSize = peerIds.size;
+    });    
 });
 
 let mySketch = new p5(SKETCH, 'sketch');
 
-peerEvents.addEventListener('join', e => {
+let pen = window.pen;
+let marker = window.marker;
+let palette = window.palette;
 
-    peerIds.add(e.peerId);
-    window.peercount.innerHTML = peerIds.size;
-    window.peerSize = peerIds.size;
+pen.addEventListener('click', e => {
+  if(window.peerSize >= 1) {
+      console.log("Pen selected");
+      window.tool = "Pen";
+  } else {
+      console.log("You need at least 1 other person on the network to access this tool");
+  }
 });
 
-peerEvents.addEventListener('leave', e => {
-
-    peerIds.delete(e.peerId);
-    window.peercount.innerHTML = peerIds.size;
-    window.peerSize = peerIds.size;
+marker.addEventListener('click', e => {
+    if(window.peerSize >= 2) {
+        console.log("Marker selected");
+        window.tool = "Marker";
+    } else {
+        console.log("You need at least 2 other people on the network to access this tool");
+    }
 });
 
+palette.addEventListener('click', e => {
+    if(window.peerSize >= 3) {
+        console.log("Palette selected");
+        window.tool = "Palette";
+    } else {
+        console.log("You need at least 3 other people on the network to access this tool");
+    }
+});
--- tool/scripts/sketch.js
+++ tool/scripts/sketch.js
@@ -1,16 +1,14 @@
 let sketch = p => {
 
-
     p.setup = () => {
         p.createCanvas(500,500);
+
     }
 
     p.draw = () => {
         p.background(159, 194, 178);
-        let peer = window.peerSize;
-        p.textSize(32);
-        p.text(peer, p.width/2, p.height/2, 25);
     }
+
 }
 
 export default sketch;
