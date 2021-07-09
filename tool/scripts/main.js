89b123d6d4fb0646039957509bfb43f375518eab
Advait Kalakkad <advait.kalakkad@gmail.com>
2021-07-09 11:00:33 -0700

    selection state added

--- tool/scripts/main.js
+++ tool/scripts/main.js
@@ -5,12 +5,14 @@ let peerEvents = beaker.peersockets.watch();
 let toolbar = document.querySelector('.tools-container');
 let pen = window.pen;
 let marker = window.marker;
-let palette = window.palette;
+let eraser = window.eraser;
 
 let slider = window.slider;
 let sizeCount = window.sizeCount;
 
 window.toolSize = 25;
+window.tool = 'None';
+
 
 beaker.hyperdrive.getInfo(window.location.href).then(info => {
 
@@ -46,23 +48,15 @@ beaker.hyperdrive.getInfo(window.location.href).then(info => {
             toolbar.children[i].classList.remove('unlocked');
         }
     });    
-
-    if(info.peers >= 1) {
-        window.tool = "Pen";
-    } else {
-        window.tool = "None";
-    }
 });
-
-
-
-window.tool =  'None';
 
 pen.addEventListener('click', e => {
   if(window.peerSize >= 1) {
-      console.log("Pen selected");
-      window.tool = "Pen";
-  } else {
+        console.log("Pen selected");
+        window.tool = "Pen";
+    
+        setSelection("pen"); 
+    } else {
       console.log("You need at least 1 other person on the network to access this tool");
   }
 });
@@ -71,15 +65,20 @@ marker.addEventListener('click', e => {
     if(window.peerSize >= 2) {
         console.log("Marker selected");
         window.tool = "Marker";
+
+        setSelection("marker"); 
+
     } else {
         console.log("You need at least 2 other people on the network to access this tool");
     }
 });
 
-palette.addEventListener('click', e => {
+eraser.addEventListener('click', e => {
     if(window.peerSize >= 3) {
-        console.log("Palette selected");
-        window.tool = "Palette";
+        console.log("Eraser selected");
+        window.tool = "Eraser";
+
+        setSelection("eraser");
     } else {
         console.log("You need at least 3 other people on the network to access this tool");
     }
@@ -90,3 +89,14 @@ slider.addEventListener('input', (e) => {
     sizeCount.innerHTML = e.target.value;
     window.toolSize = e.target.value;
 });
+
+function setSelection(target) {
+
+    toolbar.children.forEach(elt => {
+        if(elt.id === target) {
+            elt.classList.add('selected');
+        } else {
+            elt.classList.remove('selected');
+        }
+    });
+}
